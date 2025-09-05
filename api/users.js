aee8dd1fff5991fadbfa8fa809675d8a2e4963b4
azizurrbappy <azizurr.bappy@gmail.com>
2025-09-06 00:24:33 +0600

    Setup Complete

---
+++ api/users.js
@@ -0,0 +1,11 @@
+{
+  "name": "userDB",
+    "message": "successfully fetched all the user db",
+    "userData": [
+      {
+        "name": "Azizur Rahman Bappy"
+        "username": "mebappy"
+        "password": "1234"
+      }
+    ]
+}
